f5a250614f8129adebedbdf295ed3c4d9e5c02d9
silver3456 <avseliverov@gmail.com>
2018-12-25 22:55:15 -0800

    Added signUp method with JUnit, CSV, Selenium

--- src/test/java/tests/NewAccountDDT.java
+++ src/test/java/tests/NewAccountDDT.java
@@ -1,71 +1,84 @@
 package tests;
 
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(value = Parameterized.class)
 public class NewAccountDDT {
 
-    private String name;
+    private WebDriver driver;
+
+    private String firstName;
+    private String lastName;
     private String email;
-    private String phone;
-    private String gender;
     private String password;
-    private String country;
-    private boolean weeklyEmail;
-    private boolean monthlyEmail;
-    private boolean occasionalEmail;
 
     @Test
     public void newAccountTest() {
-        System.out.println("NEW RECORD: " + name + " " + email + " " + phone + " " + gender + " " + password + " " + country + " " + weeklyEmail + " " +
-                " " + monthlyEmail + " " + occasionalEmail);
+        System.out.println("NEW RECORD: " + firstName + " " + lastName + " " + email + " " + password);
+
+
+        //Define web elements
+        WebElement firstNameElement = driver.findElement(By.xpath("//*[@id='gss-signup-first-name']"));
+        WebElement lastNameElement = driver.findElement(By.xpath("//*[@id='gss-signup-last-name']"));
+        WebElement emailElement = driver.findElement(By.xpath("//*[@id ='gss-signup-email']"));
+        WebElement passwordElement = driver.findElement(By.xpath("//*[@id='gss-signup-password']"));
+        WebElement signUpButtonElement = driver.findElement(By.xpath("//*[@id='gss-signup-submit']"));
+
+
+        //Fill out the form
+        firstNameElement.sendKeys(firstName);
+        lastNameElement.sendKeys(lastName);
+        emailElement.sendKeys(email);
+        passwordElement.sendKeys(password);
+        signUpButtonElement.click();
+
 
     }
 
-
     @Parameters
     public static List<String[]> getData() {
-        return utilities.CSV.get("/Users/alexander/SDET_Files/UserAccounts.csv");
+        return utilities.CSV.get("/Users/alexander/SDET_Files/ExpediaUserAccounts.csv");
 
     }
 
     //Constructor that passes parameters to the test method
 
 
-    public NewAccountDDT(String name, String email, String phone, String gender, String password, String country,
-                         String weeklyEmail, String monthlyEmail, String occasionalEmail) {
-        this.name = name;
+    public NewAccountDDT(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.phone = phone;
-        this.gender = gender;
         this.password = password;
-        this.country = country;
+    }
 
-        if (weeklyEmail.equals("TRUE")) {
-            this.weeklyEmail = true;
-        } else {
-            this.weeklyEmail = false;
-        }
+    @Before
+    public void setUp() {
+        String webUrl = "https://www.expedia.com/";
 
-        if (monthlyEmail.equals("TRUE")) {
-            this.monthlyEmail = true;
-        } else {
-            this.monthlyEmail = false;
-        }
+        driver = utilities.DriverFactory.open("chrome");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(webUrl);
+        driver.findElement(By.xpath("//*[@id='header-account-menu']")).click();
+        driver.findElement(By.xpath("//*[@id='account-register']")).click();
 
-        if (occasionalEmail.equals("TRUE")) {
-            this.occasionalEmail = true;
-        } else {
-            this.occasionalEmail = false;
-        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
 
     }
 }
