2eed138a7032a4c36b1c1cdcdc59474657035a7f
silver3456 <avseliverov@gmail.com>
2019-02-26 22:29:07 -0800

    Add take screenshots

--- src/test/java/browser_stack/BrowserStackTestDataProvider.java
+++ src/test/java/browser_stack/BrowserStackTestDataProvider.java
@@ -1,13 +1,19 @@
 package browser_stack;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -19,7 +25,7 @@ public class BrowserStackTestDataProvider {
     public static final String URL = "http://" + USERNAME + ":" + ACCESSKEY + "@hub-cloud.browserstack.com/wd/hub";
 
     @Test(dataProvider = "browserStackTestData")
-    public void openSTM(Platform platform, String browserName, String versionName) throws MalformedURLException {
+    public void openSTM(Platform platform, String browserName, String versionName) throws IOException {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setPlatform(platform);
@@ -32,7 +38,14 @@ public class BrowserStackTestDataProvider {
         WebDriver driver = new RemoteWebDriver(browserStackURL, capabilities);
 
         driver.get("https://www.expedia.com");
-        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+
+        //Take screenshots
+        driver = new Augmenter().augment(driver);
+        File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile, new File ("/Users/alexander/SDET_Files/screenshots/screenshot.png"));
+
+
         String actualUrl = driver.getCurrentUrl();
         String expectedURL = "https://www.expedia.com/";
 
